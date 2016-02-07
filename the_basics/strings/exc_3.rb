cbede28ebecf0a858ebf923675176eef389d00c0
Rehan Pochkhanawala <rehanp@msn.com>
2016-02-07 23:41:21 +0000

    Update Strings excercises

---
+++ the_basics/strings/exc_3.rb
@@ -0,0 +1,11 @@
+movies = { CitizenLame: 1988,
+           SnakesonaLame: 1989,
+           CrazyLame: 1990,
+           IminLame: 1991,
+           ToyLame: 1992 }
+
+puts movies[:CitizenLame]
+puts movies[:SnakesonaLame]
+puts movies[:CrazyLame]
+puts movies[:IminLame]
+puts movies[:ToyLame]
